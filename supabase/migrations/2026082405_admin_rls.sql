1dd31a7425c162f636ccbcd2cd9b968e48d0fe8d
muhqosaswardani <muhqosaswardani@gmail.com>
2026-08-24 10:48:15 +0700

    chore(db): add admin RLS policy for users table

---
+++ supabase/migrations/2026082405_admin_rls.sql
@@ -0,0 +1,17 @@
+-- Migration 2026082405_admin_rls.sql
+-- Allow authenticated admin & users to view user list in Admin Dashboard
+
+CREATE POLICY "Allow authenticated to view all users"
+    ON public.users FOR SELECT
+    TO authenticated
+    USING (true);
+
+CREATE POLICY "Allow authenticated to update all users"
+    ON public.users FOR UPDATE
+    TO authenticated
+    USING (true);
+
+CREATE POLICY "Allow authenticated to delete users"
+    ON public.users FOR DELETE
+    TO authenticated
+    USING (true);
